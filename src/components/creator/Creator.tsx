3fe60c24ece52185653910e6b02348fdf65f7337
strange <38800238+rivasjoaquin02@users.noreply.github.com>
2023-11-04 11:42:12 -0400

    fixed the creator component

--- src/components/creator/Creator.tsx
+++ src/components/creator/Creator.tsx
@@ -9,55 +9,55 @@ const LIMIT = 4;
 type Coord = [number, number];
 
 function CreatorCell({
-    color,
-    onClick,
+	color,
+	onClick,
 }: {
-    color: Color;
-    onClick: () => void;
+	color: Color;
+	onClick: () => void;
 }) {
-    const style = { background: color };
-    return (
-        <button
-            type="button"
-            className="creator__cell"
-            style={style}
-            onClick={onClick}
-        ></button>
-    );
+	const style = { background: color };
+	return (
+		<button
+			type="button"
+			className="creator__cell"
+			style={style}
+			onClick={onClick}
+		></button>
+	);
 }
 
 function Creator() {
-    const [coords, setCoords] = useState<Array<Coord>>([]);
+	const [coords, setCoords] = useState<Array<Coord>>([]);
 
-    const handleToggleCell = (row: number, col: number) => {
-        if (coords.length === LIMIT) return;
+	const handleToggleCell = (row: number, col: number) => {
+		if (coords.length === LIMIT) return;
 
-        const newCoords = coords.filter(([x, y]) => x !== row || y !== col);
-        newCoords.push([row, col]);
-        setCoords(newCoords);
-    };
+		const newCoords = coords.filter(([x, y]) => x !== row || y !== col);
+		newCoords.push([row, col]);
+		setCoords(newCoords);
+	};
 
-    const board = useMemo(() => {
-        const board = createBoard(4, 4);
-        for (const [x, y] of coords) board[x]![y] = "green";
-        return board;
-    }, [coords]);
+	const board = useMemo(() => {
+		const board = createBoard({ ROWS: 4, COLS: 4 });
+		for (const [x, y] of coords) board[x]![y] = "green";
+		return board;
+	}, [coords]);
 
-    return (
-        <div className="creator">
-            {board.map((row, rowIdx) => (
-                <div className="creator__row" key={rowIdx}>
-                    {row.map((cellColor, cellIdx) => (
-                        <CreatorCell
-                            key={cellIdx}
-                            color={cellColor}
-                            onClick={() => handleToggleCell(rowIdx, cellIdx)}
-                        />
-                    ))}
-                </div>
-            ))}
-        </div>
-    );
+	return (
+		<div className="creator">
+			{board.map((row, rowIdx) => (
+				<div className="creator__row" key={rowIdx}>
+					{row.map((cellColor, cellIdx) => (
+						<CreatorCell
+							key={cellIdx}
+							color={cellColor}
+							onClick={() => handleToggleCell(rowIdx, cellIdx)}
+						/>
+					))}
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default Creator;
